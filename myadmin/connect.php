42fa218f1ddd857c48b92cdde80a52cb74c762a9
dipesh227 <dipeshjoshi227@gmail.com>
2020-10-15 13:24:45 +0530

    nodata

--- myadmin/connect.php
+++ myadmin/connect.php
@@ -3,5 +3,6 @@ $draiver_host_db = "mysql:host=localhost;dbname=traningwebsite;port=3307";
 $user = "root";
 $password = "";
 $port = 3307;
-$conn = new PDO($draiver_host_db, $user, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $conn = new PDO($draiver_host_db, $user, $password);
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
