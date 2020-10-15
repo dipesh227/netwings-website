73d882e269c9e22c0d49e47ea94ed9e345154a5f
Dipesh Joshi <54115006+dipesh227@users.noreply.github.com>
2020-10-15 22:38:29 +0530

    chek

--- myadmin/connect.php
+++ myadmin/connect.php
@@ -5,4 +5,5 @@ $password = "";
 $port = 3307;
 // $conn = new PDO($draiver_host_db, $user, $password);
 // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $conn = new PDO("mysql://ugxpcosnx8mmz8wu:RM3HabNU2Tuj3S9ilTlZ@buyyvcnf8vyc0fzsfqch-mysql.services.clever-cloud.com:3306/buyyvcnf8vyc0fzsfqch");
 ?>
