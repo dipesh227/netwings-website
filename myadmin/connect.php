e9eed89e9ecdb1b94bd79abc3fedc6919901340e
dipesh227 <dipeshjoshi227@gmail.com>
2020-10-15 13:22:34 +0530

    data

--- myadmin/connect.php
+++ myadmin/connect.php
@@ -1,12 +1,7 @@
 <?php
-$draiver_host_db="mysql:host=localhost;dbname=traningwebsite;port=3307";
-$user="root";
-$password="";
-$port=3307;
-try {
-    $conn=new PDO($draiver_host_db,$user,$password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo"connection failed";
-}
-?>
+$draiver_host_db = "mysql:host=localhost;dbname=traningwebsite;port=3307";
+$user = "root";
+$password = "";
+$port = 3307;
+$conn = new PDO($draiver_host_db, $user, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
