fa7ebbff50f4b5f0ff795fdd4fe3f76c2b992ffa
dipesh227 <dipeshjoshi227@gmail.com>
2020-10-17 18:02:50 +0530

    mb

--- learnandearn.php
+++ learnandearn.php
@@ -9,12 +9,12 @@
     Corporate training is extremely valuable for any business organization as it not only provides benefits to the individual but also allows the business company to continue their effective Run and enhance their productivity. Any business owner should understand that providing training to their corporate employees can certainly drive their business forward and regular corporate training can also result in skill development of the employees allowing them to contribute more to the organization. Some of the various benefits of Corporate training include:
     <h1 class="m-4 font-weight-bold"> Vision and Mission</h1>
     <ul>
-      <li>It allows the company to keep up with the industry changes so that their company do not left behind and keep a competitive edge over others.</li>
-      <li>It helps to company to be in touch with all the latest technology available so that they can utilize its full potential and achieve growth.</li>
-      <li>Corporate training can also help the company to stay ahead of their competitors as standing still would result in killing the business.</li>
-      <li>Regular staff training can also allow the company to identify their weaknesses and skill gap and take necessary measures to resolve them and increase the performance of the work force.</li>
-      <li>Corporate training also maintenance the knowledge and skills of the employees and also offers and incentive to learn and put the new skills to the practice.</li>
-      <li>It increases job satisfaction level of the employees who become happier due to their increasing contribution to the company which also provides internal promotion opportunities along with attracting new talents.</li>
+      <li class="mb-3">It allows the company to keep up with the industry changes so that their company do not left behind and keep a competitive edge over others.</li>
+      <li class="mb-3">It helps to company to be in touch with all the latest technology available so that they can utilize its full potential and achieve growth.</li>
+      <li class="mb-3">Corporate training can also help the company to stay ahead of their competitors as standing still would result in killing the business.</li>
+      <li class="mb-3">Regular staff training can also allow the company to identify their weaknesses and skill gap and take necessary measures to resolve them and increase the performance of the work force.</li>
+      <li class="mb-3">Corporate training also maintenance the knowledge and skills of the employees and also offers and incentive to learn and put the new skills to the practice.</li>
+      <li class="mb-3">It increases job satisfaction level of the employees who become happier due to their increasing contribution to the company which also provides internal promotion opportunities along with attracting new talents.</li>
 
     </ul>
 
