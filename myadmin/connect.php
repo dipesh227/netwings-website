c13a04ae858741e3b8412547298b5b6d00f6a810
Dipesh Joshi <54115006+dipesh227@users.noreply.github.com>
2022-10-30 17:55:01 +0530

    Update connect.php

--- myadmin/connect.php
+++ myadmin/connect.php
@@ -1,5 +1,5 @@
 <?php
-$draiver_host_db = "mysql:host=buyyvcnf8vyc0fzsfqch-mysql.services.clever-cloud.com;dbname=buyyvcnf8vyc0fzsfqch;port=3306";
+$draiver_host_db = "mysql:host=builotqfurta4hn3tpbs-mysql.services.clever-cloud.com;dbname=builotqfurta4hn3tpbs;port=3306";
 $user = "ugxpcosnx8mmz8wu";
 $password = "RM3HabNU2Tuj3S9ilTlZ";
 try {
